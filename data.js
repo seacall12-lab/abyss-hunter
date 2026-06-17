2d21f5b9d21f9fa8e9711e2726f739db1bf47cdc
seacall12-lab <seacall12@gmail.com>
2026-06-17 15:13:58 +0900

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,7 @@
   loadContentDataFallback();
 
   const CONFIG = {
-    GAME_VERSION: "0.5.6",
+    GAME_VERSION: "0.5.8",
     SAVE_VERSION: 5,
     STORAGE_KEY: "abyssHunter.save",
     AUTOSAVE_INTERVAL_MS: 5000,
